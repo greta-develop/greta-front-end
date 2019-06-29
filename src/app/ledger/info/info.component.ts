c18c0ebb16b4b7ff0fc9201a8985438b30f0a098
kys6879 <kys6879@naver.com>
2019-06-30 01:25:56 +0900

    modify

--- src/app/ledger/info/info.component.ts
+++ src/app/ledger/info/info.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { finalize, take, map, tap } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
-
+import { WavesModule, TableModule, InputsModule, MdbTableDirective } from 'angular-bootstrap-md';
 import { Router } from '@angular/router';
+
 import * as $ from 'jquery';
 
 declare var $: any;
+
 
 @Component({
   selector: 'app-info',
@@ -17,39 +19,139 @@ declare var $: any;
 })
 export class InfoComponent implements OnInit {
 
+  @ViewChild(MdbTableDirective) mdbTable: MdbTableDirective;
+  elements: any = [];
+  headElements = ['id', 'flag', 'Contents', 'Type', 'Details', 'Receipt', 'Amount', 'Balance', 'Date', 'Reply'];
+
+  searchText: string = '';
+  previous: string;
+
+
+
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router,
     private toastr: ToastrService) { }
 
-  tranList: any[];
-  ngOnInit() {
-    // this.queryTranList();
+  @HostListener('input') oninput() {
+    this.searchItems();
   }
 
-  queryTranList() {
-    let token = localStorage.getItem('token');
+  tranList: any[];
+  url: any;
 
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    });
-    let options = {
-      headers: headers
-    };
+  ngOnInit() {
+    // this.queryTranList();
+    this.queryGroups() //시도한 유저의 url 을 얻어서
+      .then((ret) => {
+        this.url = ret;
+        this.queryTranList(this.url)
+          .then((ret: any) => {
+            for (let i = 1; i <= ret.length; i++) {
+              this.elements.push(
+                {
+                  id: i.toString(),
+                  flag: "dummy",
+                  contents: ret[i - 1].content,
+                  type: ret[i - 1].type,
+                  details: ret[i - 1].detail,
+                  receipt: ret[i - 1].receipt,
+                  amount: ret[i - 1].amount,
+                  balance: ret[i - 1].balance,
+                  date: ret[i - 1].date,
+                  reply: ret[i - 1].reply,
+                });
+            }
 
-    console.log(token);
+            this.mdbTable.setDataSource(this.elements);
+            this.elements = this.mdbTable.getDataSource();
+            this.previous = this.mdbTable.getDataSource();
+          })
+      })
 
-    let url = "hanyang";
-    this.http.get(`${environment.server.url}/api/share/${url}`, options)
-      // .pipe(
-      //   finalize(() => this.ngxService.stop())
-      // )
-      .subscribe((ret: any) => {
-        this.toastr.success("정보 가져오기 성공");
-        console.log("[info.component.ts] $queryTranList () ====> ret", ret);
-      }, e => {
-        this.toastr.error("로그인 실패. 정보를 확인해주세요");
-        console.error("[info.component.ts] $queryTranList () ====> ret", e);
+
+  }
+
+  searchItems() {
+    const prev = this.mdbTable.getDataSource();
+
+    if (!this.searchText) {
+      this.mdbTable.setDataSource(this.previous);
+      this.elements = this.mdbTable.getDataSource();
+    }
+
+    if (this.searchText) {
+      this.elements = this.mdbTable.searchLocalDataBy(this.searchText);
+      this.mdbTable.setDataSource(prev);
+    }
+  }
+
+  showListMenu() {
+    console.log("show!");
+    $('#list-menu-modal').modal('show');
+  }
+
+  queryGroups() {
+    return new Promise((res, rej) => {
+      let token = localStorage.getItem('token');
+
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
       });
+      let options = {
+        headers: headers
+      };
+
+      console.log(token);
+
+      this.http.get(`${environment.server.url}/api/groups`, options)
+        // .pipe(
+        //   finalize(() => this.ngxService.stop())
+        // )
+        .subscribe((ret: any) => {
+          this.toastr.success("정보 가져오기 성공");
+          this.url = ret[0].alias_url;
+          console.log("[info.component.ts] $queryGroups () ====> ret", ret);
+          console.log("[info.component.ts] $queryGroups () ====> url", this.url);
+
+          res(this.url);
+        }, e => {
+          this.toastr.error("정보 가져오기 실패");
+          console.error("[info.component.ts] $queryGroups () ====> ret", e);
+          rej(e);
+        });
+    })
+
+  }
+
+  queryTranList(url: any) {
+    return new Promise((res, rej) => {
+      let token = localStorage.getItem('token');
+
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
+      let options = {
+        headers: headers
+      };
+
+      console.log(token);
+
+      this.http.get(`${environment.server.url}/api/share/${url}`, options)
+        // .pipe(
+        //   finalize(() => this.ngxService.stop())
+        // )
+        .subscribe((ret: any) => {
+          this.toastr.success("트랜젝션 가져오기 성공");
+          console.log("[info.component.ts] $queryTranList () ====> ret", ret);
+          res(ret);
+        }, e => {
+          this.toastr.error("트랜젝션 가져오기 실패. 정보를 확인해주세요");
+          console.error("[info.component.ts] $queryTranList () ====> ret", e);
+          rej(e);
+        });
+    })
+
   }
 
 }
